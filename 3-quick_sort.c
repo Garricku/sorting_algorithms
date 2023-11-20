f31012f773f6c716450c757fd3d441feeb5f25f4
Garrick Larson Potts <ricki.glp@gmail.com>
2023-11-20 06:32:09 -0800

    betty task 3

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,32 +1,7 @@
 #include "sort.h"
 
 /**
- * sort_array - Sort an array to have smaller values first, bigger values last.
- * @start_index: The starting index of the array to be sorted.
- * @end_index: The ending index of the array to be sorted.
- *
- * Return: Nothing/void
- *
- * void sort_array(int *array, int start_index, int end_index)
- * {
- * 	int i = start_index, n = end_index, 
- *	size_t size = (size_t)n + 1;
- *
- *	if (array == NULL)
- *		return;
- *	if (start_index == < 0 || end_index == < 0)
- *		return;
- *	for (i; i <= n; i++)
- *	{
- *		if (array[i] > array[n])
- *			swap_value(array[i], array[n]);
- *		print_array(array, size);
- *	}
- * }
- */
-
-/**
- * quick_sort - Sorts an array of integers in ascending order using the Quick 
+ * quick_sort - Sorts an array of integers in ascending order using the Quick
  * sort algorithm.
  * @array: The array that needs to be sorted.
  * @size: The size of the array.
@@ -54,7 +29,7 @@ void quick_sort(int *array, size_t size)
 		for (i = 0; i < total_size - 1; i++)
 		{
 			/* Swap the values.*/
-			if ( flag == 0 && pivot > array[j])
+			if (flag == 0 && pivot > array[j])
 			{
 				temp = j;
 				array[0] = array[j];
